eae917fd5e0eb8a42eaef056a619153827602360
NirajPatil <nirajpatil@22400.com>
2023-02-07 22:31:12 +0530

    creating a child class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,4 +7,15 @@ class A
     {
       System.out.println("Invoking method from class A");
     }  
-  }  
+  }
+//Creating a child class
+class B extends A
+{
+  //defining the same method as in the parent class
+  void meth()
+  {
+    System.out.println("Method is overridden in Extendend class B");
+  }
+}
+
+
