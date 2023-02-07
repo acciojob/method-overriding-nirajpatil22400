f33e6cee7e145518d639144aeb773ee386efa81f
Niraj Patil <112927422+nirajpatil22400@users.noreply.github.com>
2023-02-07 21:34:57 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
-public class Main {
-  public string
+class A
+  {  
+    //defining a method  
+    void meth()
+    {
+      System.out.println("Invoking method from class A");
+    }  
+  }  
+  //Creating a child class  
+  class B extends A
+  {  
+    //defining the same method as in the parent class  
+    void meth()
+    {
+    System.out.println("Method is overridden in Extendend class B");
+    }
+  }  
+
+public class Main 
+{
+  public static void main(String args[])
+    {  
+      B obj = new B();//creating object  
+      obj.meth();//calling method  
+    }   
 }
