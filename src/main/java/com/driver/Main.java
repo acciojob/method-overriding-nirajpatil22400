2e8a7fb801cdaba7c2bf0c9e80277a80697515f6
NirajPatil <nirajpatil@22400.com>
2023-02-07 22:28:31 +0530

    defining a method

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,21 +8,3 @@ class A
       System.out.println("Invoking method from class A");
     }  
   }  
-  //Creating a child class  
-  class B extends A
-  {  
-    //defining the same method as in the parent class  
-    void meth()
-    {
-    System.out.println("Method is overridden in Extendend class B");
-    }
-  }  
-
-public class Main 
-{
-  public static void main(String args[])
-    {  
-      B obj = new B();//creating object  
-      obj.meth();//calling method  
-    }   
-}
